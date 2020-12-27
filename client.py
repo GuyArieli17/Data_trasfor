12fe3c4cd16ca182013c13058b92be4c929f5d02
Amit-Nadam <73649320+Amit-Nadam@users.noreply.github.com>
2020-12-27 23:57:07 +0200

    Add files via upload
    files from Amit

--- client.py
+++ client.py
@@ -1,56 +1,46 @@
 import socket
-import struct
-import sys
-import threading
-from _thread import start_new_thread
-from termcolor import cprint
 
-magic_cookie = 0xfeedbeef
-offer_msg_type = 0x2
-BUFFER_SIZE = 2048
-port = 13117
-broadcast_address = ('255.255.255.255', port)
-team_name = b"IDO ROM <3"
-id = 0
 
-class client:
-    def __init__(self,IP,PORT):
-        """
-        :param server_name:
-        :param server_ip:
-        :param server_port:
-        """
-        # self.server_name = server_name
-        self.server_port = PORT
-        self.server_ip = IP
-        self.listen_message = "Client started, listening for offer requests..."
-        start_new_thread(self.listen_to_broadcast,())
+class Client:
+    MAX_TALK_SIZE = 2048
 
-    def create_tcp_connection(self):
-        pass
+    def __init__(self, server_name, server_ip, server_port):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.server_ip = server_ip
 
-    def listen_to_broadcast(self):
-        """
-        :return:
-        """
-        # create socket
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # bind socket with addr ('',port_num)
-        self.client_socket.bind(('', port))
-        # print message
-        cprint(self.listen_message,'white', 'on_grey')
-        # get data, addr from server
-        data, addr = self.client_socket.recvfrom(1024)
-        # strip all from data
-        magic_cookie_byte = data[:4]
-        message_type_byte = data[4]
-        port_tcp_byte = data[5:]
-        # convert to int (can work with)
-        magic_cookie = int.from_bytes(magic_cookie_byte, byteorder='big', signed=False)
-        port_num = int.from_bytes(port_tcp_byte, byteorder='big', signed=False)
-        # print all info
-        print('Magic Cookie: ' + str(magic_cookie))
-        print('Message Type: ' + str(message_type_byte))
-        print('Port: ' + str(port_num))
-        # call create tcp connection
-        self.create_tcp_connection()
+    def broadcast_client(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.listen(13117)  # port 13117 of UDP
+        print('Client started, listening for offer request...')
+        broadcast_message, server_address = client_socket.recvfrom(self.MAX_TALK_SIZE)
+        if broadcast_message["Magic coockie"] == 0xfeedbeef:
+            self.server_port = broadcast_message["Server port"]
+            self.client_tcp()
+            client_socket.close()
+        client_socket.close()
+
+    def client_tcp(self):
+        clientSocket = socket(socket.AF_INET, socket.SOCK_STREAM)
+        counter = 0
+        while True:
+            clientSocket.connect((self.serverName, self.serverPort))
+            message = "I want to send you message"
+            clientSocket.send(message)
+            receive_message = clientSocket.recv(self.MAX_TALK_SIZE)
+            if receive_message == "Type your team name:\n":
+                counter += 1
+                print(receive_message)
+                message = input()
+                clientSocket.send(message)
+                receive_message = clientSocket.recv(self.MAX_TALK_SIZE)
+                if receive_message[:7] == "Welcome":
+                    while len(receive_message) == 0: # don't receive a message
+                        message = input()
+                        clientSocket.send(message)
+                    clientSocket.close()
+                    self.end_game()
+
+    def end_game(self):
+        message = "Server disconnected, listening for offer requests..."
+        self.broadcast_client()
--- server.py
+++ server.py
@@ -1,92 +1,142 @@
 import socket
+import _thread
 import time
-import threading
-from termcolor import colored, cprint
-from _thread import start_new_thread
-
-class server:
-
-    def __init__(self,NAME,IP,PORT,broadcast_port = 13117):
-        """
-        :param NAME: the name fo the server
-        :param IP: ip of the server
-        :param PORT: the port we will use to brodcast (use in the future)
-        """
-        #all parms from arg - save in self
-        self.server_name = NAME
-        self.server_ip = IP
-        self.server_port = PORT
-        self.broadcast_port = broadcast_port
-        # information we will print
-        self.booting_message = "Server started, listening on IP address " + str(self.server_ip)
-        # booting server all we need for connection
-        self.server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        thread = threading.Thread(target=self.mannager,args=())
-        thread.start()
+import random
+import json
+#from client import Client
 
 
-    def mannager(self):
-        # while True:
-        self.offer_announcements_udp()
-        self.game_mode()
+class Server:
+    MAX_TALK_SIZE = 2048
 
-    def game_mode(self):
-        print('_______________')
-        print('In Game Mode')
-        print('_______________')
-        pass
+    def __init__(self, server_name, server_ip, server_port):
+        self.server_name = server_name
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.client_dict = {}  # Format of {ip: [port, socket, team_name, group_number]}
+        self.groups = {'Group 1': [], 'Group 2': []}
+        self.counters = [0, 0]  # group1, group2 counters
+        self.start_time = 0
 
-    def offer_announcements_udp(self):
-        print('_______________')
-        print('In Brodcast Mode')
-        print('_______________')
-        # color text
-        cprint(self.booting_message, 'grey', 'on_white')
-        # get time when started brodacsting
-        start_listining_time = time.time()
-        current_time =start_listining_time
-        self.bordcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        # Enable port reusage
-        self.bordcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        # Enable broadcasting mode
-        self.bordcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        # Set a timeout so the socket does not block
-        self.bordcast_socket.settimeout(0.2)
-        # the magic_cookie we will check the client really want to connect (kind of message)
-        magic_cookie = "feedbeef"
-        # the kind of the massage (connection) offer
-        message_type = "02"
-        # convert all message to byte
-        magic_cookie_bytes = bytes.fromhex(magic_cookie)
-        message_type_bytes = bytes.fromhex(message_type)
-        server_port_bytes = self.server_port.to_bytes(2, byteorder='big')
-        # combaine all the message in bye as ordered
-        message = magic_cookie_bytes + message_type_bytes + server_port_bytes
-        # run for 10 sec
-        while current_time - start_listining_time < 10:
-            # evrey secound ~ send bordcast
-            self.bordcast_socket.sendto(message, ('<broadcast>', self.broadcast_port))
+    def startServer(self):
+        print(f"Server started, listening on IP address {self.server_ip}")
+        _thread.start_new_thread(self.broadcast_sending())
+        _thread.start_new_thread(self.tcp_connection())
+
+    def broadcast_sending(self):
+        while len(self.client_dict) < 4:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            # the magic_cookie we will check the client really want to connect (kind of message)
+            magic_cookie = "feedbeef"
+            # the kind of the massage (connection) offer
+            message_type = "02"
+            # convert all message to byte
+            magic_cookie_bytes = bytes.fromhex(magic_cookie)
+            message_type_bytes = bytes.fromhex(message_type)
+            server_port_bytes = self.server_port.to_bytes(2, byteorder='big')
+            # combaine all the message in bye as ordered
+            message = magic_cookie_bytes + message_type_bytes + server_port_bytes
+            server_socket.sendto(message, ('<broadcast>', 13117))
             time.sleep(1)
-            # get current time
-            current_time = time.time()
-        #end the connection
-        # print('Close connection')
+        self.start_time = time.time()
 
+    def tcp_connection(self):
+        self.server_socket.listen(self.server_port)
+        steps = 0
+        start_time = 0
+        while True:
+            connection_socket, (ip, port) = self.server_socket.accept()
+            if steps == 0:  # start the tcp connection
+                print(f"Received offer from {ip},attempting to connect...")
+                client = Client(ip, port, connection_socket)
+                self.client_list[ip] = list()
+                self.client_list[ip].append(port)
+                self.client_list[ip].append(connection_socket)
+                message = "Type your team name:\n"
+                connection_socket.send(message)
+                steps += 1
+            if steps == 1:  # get the team name
+                team_name = connection_socket.recv(self.MAX_TALK_SIZE)[:-1]  # [:-1] remove the \n
+                self.client_list[ip].append(team_name)
+                self.add_to_random_group(ip, team_name)
+                now_time = time.time()
+                while now_time - self.start_time < 10:
+                    pass  # wait
+                start_time = self.start_game()
+                # start game and countering the amount of keys of each group
+                while time.time() - start_time < 10:
+                    self.update_counters(connection_socket, ip)
+                print(self.end_game())
+            self.disconnect_client()
+            connection_socket.close()
+        self.server_socket.close()
 
-    # def boot_server(self):
-    #     # print message
-    #     print(self.booting_message)
-    #     self.start_boot_time = time()
-    #     while True:
-    #         current_time = time()
-    #         delta_time = current_time - self.start_boot_time
-    #         if delta_time > 15:
-    #             break
-    #         try:
-    #             server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #             server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #             server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    #             server_socket_tcp.settimeout(10)
+    def add_to_random_group(self, ip, team_name):
+        group_num = random.randint(1, 3)
+        if group_num == 1:
+            if len(self.groups[f'Group {group_num}']) < 2:
+                self.groups['Group 1'].append(team_name)
+                self.client_dict[ip].appeand(1)
+            else:
+                self.groups['Group 2'].append(team_name)
+                self.client_dict[ip].appeand(2)
+        else:
+            if len(self.groups[f'Group {group_num}']) < 2:
+                self.groups['Group 2'].append(team_name)
+                self.client_dict[ip].appeand(2)
+            else:
+                self.groups['Group 1'].append(team_name)
+                self.client_dict[ip].appeand(1)
+
+    def start_game(self):
+        # welcome message
+        for id in self.client_dict.keys():
+            connectionSocket = self.client_dict[id][1]  # socket
+            group1 = self.groups["Group 1"]
+            group2 = self.groups["Group 2"]
+            message = "Welcome to Keyboard Spamming Battle Royale.\n"
+            message += "Group 1:\n==\n"
+            for team1 in self.group1.keys():
+                message += str(team1) + '\n'
+            message += "Group 2:\n==\n"
+            for team2 in self.group2.keys():
+                message += str(team2) + '\n'
+            message += "Start pressing keys on your keyboard as fast as you can!!"
+            connectionSocket.send(message)
+        return time.time()
+
+    def update_counters(self, connection_socket, ip):
+        if connection_socket.recv() > 1:
+            if self.client_dict[ip][3] == 1:
+                self.counters[0] += 1
+            else:
+                self.counters[1] += 1
+
+    def end_game(self):
+        group1 = self.groups["Group 1"]
+        group2 = self.groups["Group 2"]
+        message = "Game over! \n"
+        message += f"Group 1 typed in {self.counters[0]} characters. Group 2 typed in {self.counters[1]} characters.\n"
+        if self.counters[0] > self.counters[1]:
+            message += "Group 1 wins! \n\n"
+            message += "Congratulations to the winners:\n=="
+            message += self.groups['Group 1'][0] + "\n"
+            message + self.groups['Group 1'][1] + "\n"
+        else:
+            message += "Group 2 wins! \n\n"
+            message += "Congratulations to the winners:\n=="
+            message += self.groups['Group 2'][0] + "\n"
+            message + self.groups['Group 2'][1] + "\n"
+        return message
+
+    def disconnect_client(self):
+        message = "Game over, sending out offer requests..."
+        print(message)
+        # restart the values
+        self.client_dict = {}  # Format of {ip: [port, socket, team_name, group_number]}
+        self.groups = {'Group 1': [], 'Group 2': []}
+        self.counters = [0, 0]  # group1, group2 counters
+        self.start_time = 0
+        self.startServer()
