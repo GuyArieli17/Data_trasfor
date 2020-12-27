aa1f899d62611da53224190b9bc618e0237e78c9
Guy Arieli <guyarieli17@gmail.com>
2020-12-28 00:43:13 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import socket
 import time
 import threading
+from random import random
+
 from termcolor import colored, cprint
 from _thread import start_new_thread
+from threading import Lock
 
 class server:
 
@@ -19,32 +22,40 @@ class server:
         self.broadcast_port = broadcast_port
         # information we will print
         self.booting_message = "Server started, listening on IP address " + str(self.server_ip)
+        # resat number of client to zero(just started the server)
+        self.num_of_client = 0
+        # create a lock we will use
+        self.lock = Lock()
         # booting server all we need for connection
-        self.server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         thread = threading.Thread(target=self.mannager,args=())
         thread.start()
 
-
-    def mannager(self):
-        # while True:
-        self.offer_announcements_udp()
-        self.game_mode()
+    def handle_client(self,data):
+        # get the data from the client(is team name)
+        client_team_name = str(data.recv(1024), 'utf-8')
+        # Todo: send game info to user
+        # Todo: add all user game
+        pass
 
     def game_mode(self):
         print('_______________')
         print('In Game Mode')
         print('_______________')
-        pass
+        while True:
+            # establish connection with client
+            data, addr = self.tcp_connection_socket.accept()
+            self.lock.acquire()
+            #inc number of particants
+            self.num_of_client += 1
+            self.lock.release()
+            # Start a new thread and return its identifier
+            start_new_thread(self.handle_client, (data,))
+
 
     def offer_announcements_udp(self):
         print('_______________')
         print('In Brodcast Mode')
         print('_______________')
-        # color text
-        cprint(self.booting_message, 'grey', 'on_white')
         # get time when started brodacsting
         start_listining_time = time.time()
         current_time =start_listining_time
@@ -73,20 +84,16 @@ class server:
             # get current time
             current_time = time.time()
         #end the connection
+        self.bordcast_socket.close()
         # print('Close connection')
 
-
-    # def boot_server(self):
-    #     # print message
-    #     print(self.booting_message)
-    #     self.start_boot_time = time()
-    #     while True:
-    #         current_time = time()
-    #         delta_time = current_time - self.start_boot_time
-    #         if delta_time > 15:
-    #             break
-    #         try:
-    #             server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #             server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #             server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    #             server_socket_tcp.settimeout(10)
+    def mannager(self):
+        # create tcp connection to client to join
+        self.tcp_connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_connection_socket.bind((self.server_ip, self.server_port))
+        # color text
+        cprint(self.booting_message, 'grey', 'on_white')
+        thread = threading.Thread(target=self.offer_announcements_udp)
+        thread.start()
+        self.tcp_connection_socket.listen(4)
+        self.game_mode()
